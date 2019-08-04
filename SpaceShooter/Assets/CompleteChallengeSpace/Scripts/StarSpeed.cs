352a60a912584c1b75036bbcdddeeeb1a8aa9aa0
Tapia_Timmothy <tapiatim24@gmail.com>
2019-08-04 17:23:41 -0400

    FINAL

---
+++ SpaceShooter/Assets/CompleteChallengeSpace/Scripts/StarSpeed.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarSpeed : MonoBehaviour
+{
+    private bool called;
+    private float speed;
+
+    private ParticleSystem ps;
+    public float hSliderValue = 1.0F;
+   
+    void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+    }
+    void Update()
+    {   
+        var main = ps.main;
+        main.simulationSpeed = hSliderValue;
+
+        if(Done_GameController.winGame == true && called == false){
+            called = true;
+            hSliderValue = 50.0f;
+        }
+    }
+}
